46d6eb60720c03ab898c1f254b59e01d1829921d
RobertPr <ProtasiukRobert@wp.pl>
2018-04-03 00:46:05 +0200

    wrzucenie htmla login i register, niedziałajacy css (test commit)

--- avoco/src/login/Login.js
+++ avoco/src/login/Login.js
@@ -4,10 +4,27 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <React.Fragment>
-        <h3>This is login page</h3>
-        <Link to="/">Go to /</Link>
-        </React.Fragment>
+        //<React.Fragment>
+        //<h3>This is login page</h3>
+        //<Link to="/">Go to /</Link>
+       // </React.Fragment>
+        <div id="Center">
+            <div id="ToLeft">
+                <div id="Logo">AVOCO</div>
+                <div id="Login">Zaloguj się</div>
+                <p>
+                    <div id="LoginDesc">i dołącz do setek ludzi<br></br>o wspólnych zainteresowaniach!</div>
+                </p>
+         </div>
+                <form id="FlexContainer_Login">
+                    <input className={styles.form} name="Email" placeholder="E-mail" />
+                    <input className={styles.form} name="Password" placeholder="Hasło" />
+                    <a id="loginButton" href="home.html">Zaloguj</a>
+                </form>
+            <div id="No_Account">Nie masz konta?
+            <a id="signUpLink" href="index.html">Zarejestruj się</a>
+            </div>
+        </div>
     )
 }
 export default Login;
--- avoco/src/register/Register.js
+++ avoco/src/register/Register.js
@@ -1,8 +1,50 @@
 import React from 'react';
 import styles from './Register.module.css';
+import { Link } from 'react-router-dom';
+
 const Register = () => {
     return (
-        <h2>This is registration page</h2>
+        <div id="Center">
+    <div id="ToLeft">
+      <div id="Logo">AVOCO</div>
+      <div id="Register">Załóż konto</div>
+      <p>
+        <div id="LoginDesc">i dołącz do setek ludzi<br></br>o wspólnych zainteresowaniach!</div>
+      </p>
+    </div>
+    <form id="FlexContainer_Login">
+      <input className={styleMedia.form} name="Name" placeholder="Imie" />
+      <input className={styleMedia.form} name="Surname" placeholder="Nazwisko" />
+      <div id="ComboBackground">
+       <select id="Combobox">
+         <option hidden>Województwo</option>
+         <option value="1">Dolnośląskie</option>
+         <option value="2">Kujawsko-pomorskie</option>
+         <option value="3">Lubelskie</option>
+         <option value="4">Lubuskie</option>
+         <option value="5">Łódzkie</option>
+         <option value="6">Małopolskie</option>
+         <option value="7">Mazowieckie</option>
+         <option value="8">Opolskie</option>
+         <option value="9">Podkarpackie</option>
+         <option value="10">Podlaskie</option>
+         <option value="11">Pomorskie</option>
+         <option value="12">Śląskie</option>
+         <option value="13">Świętokrzyskie</option>
+         <option value="14">Warmińsko-mazurskie</option>
+         <option value="15">Wielkopolskie</option>
+         <option value="16">Zachodniopomorskie</option>
+       </select>
+      </div>
+      <input className={styleMedia.form} name="Email" placeholder="E-mail" />
+      <input className={styleMedia.form} name="Password" placeholder="Hasło" />
+      <input className={styleMedia.form} name="Rep_Password" placeholder="Powtórz hasło" />
+      <a id="signUpButton" href="login.html">Załóż</a>
+    </form>
+    <div id="IsAccount">Mam już konto.
+    <a href="login.html"><button type="button" name="Login_Button">Zaloguj</button></a>
+    </div>
+  </div>
     );
 }
 export default Register;
