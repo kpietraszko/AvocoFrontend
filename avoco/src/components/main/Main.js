7acc3dde11255237f5d9dc08c6c9b64e4cd8ee26
kpietraszko <krzysztof@pietraszko.cba.pl>
2018-04-06 22:44:54 +0200

    struktura Main

--- avoco/src/components/main/Main.js
+++ avoco/src/components/main/Main.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Profile from '../profile/Profile';
+import Navbar from '../navbar/Navbar';
+import LeftPanel from '../leftPanel/LeftPanel';
+import RightPanel from '../rightPanel/RightPanel';
+import styles from './Main.module.css';
 class Main extends React.Component {
 	render = () => {
 		return (
-			// tu lewy panel
-			<div className="middleScreen">
+			<React.Fragment>
+			<Navbar/>
+			<LeftPanel/>
+			<div className={styles.middleScreen}>
 				<Switch>
 					{/* <Route exact path="/home" component={Home}/> */}
 					<Route exact path="/" component={Profile} /> {/* path="/profile" */}
 				</Switch>
 			</div>
-			// tu prawy panel
+			<RightPanel/>
+			</React.Fragment>
 		);
 	}
 }
---
+++ avoco/src/components/rightPanel/RightPanel.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class RightPanel extends Component {
+	render() {
+		return (
+			<div>
+				
+			</div>
+		);
+	}
+}
+
+export default RightPanel;
