4abc99111a0ee6bbd65b89569e40f76372852f00
Robert <Bob@DESKTOP-T5VIJL4>
2018-05-24 20:56:54 +0200

    format lokalizacji

--- avoco/src/api/event.js
+++ avoco/src/api/event.js
@@ -6,8 +6,8 @@ export const createApi = (form, groupId, location) => {
 	formData.append("EventDescription", form.eventDescription.value);
 	const dateTime = new Date(form.dateInput.value + "T" + form.timeInput.value).toISOString();
 	formData.append("EventDateTime", dateTime);
-	formData.append("EventLocationLat", location.lat);
-	formData.append("EventLocationLng", location.lng);
-	
+	formData.append("EventLocationLat", parseFloat(location.lat));
+	formData.append("EventLocationLng", parseFloat(location.lng));
+	console.log(location);
 	return axios.post(`/event/Create/${groupId}`, formData);
 }
