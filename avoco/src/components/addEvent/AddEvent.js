00a928c71f3587ee4202c699193a05a4a1a52e69
kpietraszko <krzysztof@pietraszko.cba.pl>
2018-05-24 19:06:06 +0200

    skladnia

--- avoco/src/components/addEvent/AddEvent.js
+++ avoco/src/components/addEvent/AddEvent.js
@@ -1,36 +1,29 @@
 import React, { Component } from 'react';
 import styles from './AddEvent.module.css';
 import { connect } from 'react-redux';
-import Spinner from '../../componentsStateless/spinner/Spinner'; 
+import Spinner from '../../componentsStateless/spinner/Spinner';
 import LocationPicker from 'react-location-picker';
-
 
 const defaultPosition = {
 	lat: 27.9878,
 	lng: 86.9250
-  };
+};
 
-class AddEvent extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			address: "Kala Pattar Ascent Trail, Khumjung 56000, Nepal",
-			position: ""
-		  };
-
-		this.handleLocationChange = this.handleLocationChange.bind(this);  // Bind
+class AddEvent extends Component {
+	state = {
+		address: "",
+		position: ""
 	}
 
-	handleLocationChange ({ position, address }) {
-		this.setState({ position, address }); // Set new location
+	handleLocationChange = ({ position, address }) => {
+		this.setState({ position, address });
 	}
 
-	render = () => { 
+	render = () => {
 		return (
-            <form id={styles.newEventForm}>
+			<form id={styles.newEventForm}>
 				<h1>Nowe wydarzenie w grupie Militaria</h1>
-				<input id={styles.eventNameInput} placeholder="Podaj nazwę wydarzenia" />
+				<input id={styles.eventNameInput} placeholder="Podaj nazwę wydarzenia" required />
 				<div id={styles.eventDetailsFlex}>
 					<div id={styles.dateTime}>
 						<h3>Wprowadź datę i godzinę</h3>
@@ -41,15 +34,15 @@ class AddEvent extends React.Component {
 						<textarea placeholder="Wprowadź opis wydarzenia" rows="3" ></textarea>
 					</div>
 				</div>
-                <h2 id={styles.mapHeader}>Wybierz miejsce wydarzenia</h2>
+				<h2 id={styles.mapHeader}>Wybierz miejsce wydarzenia</h2>
 				<h3>{this.state.address}</h3>
-					<LocationPicker
-						containerElement={<div id={styles.map} />}
-						mapElement={<div id={styles.mapSize} />}
-						defaultPosition={defaultPosition}
-						onChange={this.handleLocationChange}
-						radius={-1}
-					/>
+				<LocationPicker
+					containerElement={<div id={styles.map} />}
+					mapElement={<div id={styles.mapSize} />}
+					defaultPosition={defaultPosition}
+					onChange={this.handleLocationChange}
+					radius={-1}
+				/>
 				<input id={styles.createEventButton} className="submitButtonGreen" type="submit" value="Utwórz" />
 			</form>
 		);
